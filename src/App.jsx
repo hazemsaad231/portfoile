21816830b2589252aa4d7dd9477e19194336aa78
hazemsaad231 <166450789+hazemsaad231@users.noreply.github.com>
2025-01-30 13:21:55 +0200

    nn

--- src/App.jsx
+++ src/App.jsx
@@ -5,31 +5,15 @@ import Skills from './components/skills/skills'
 import Projects from './components/projects/projects'
 import Experience from './components/experience/experience'
 import Contact from './components/contact/Contact'
-import { useState, useEffect } from 'react'
-import Load from './components/load'
+
 
 function App() {
 
-const [loading, setLoading] = useState(true); 
 
-useEffect(() => {
-  // بعد تحميل الموقع بـ 2 ثانية، يخفي اللودر
-  const timer = setTimeout(() => {
-    setLoading(false);
-  }, 200);
-
-  return () => clearTimeout(timer);
-}, []);
   
   return (
     <>
-    {loading ? 
-      (
-        <div className="flex justify-center items-center h-screen bg-white">
-          <Load/>
-        </div>)
-    :
-(
+    
 <div>
 <NavBar/>
 <Home/>
@@ -40,7 +24,6 @@ useEffect(() => {
 <Contact/>
 </div>
 
-)}
     </>
   )
 }
